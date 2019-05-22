657f8d4b03e9ee6c75e881e952754590dfc2c00c
Wyxal <abottan6@gmail.com>
2019-05-22 11:14:13 +0200

    Date excercise

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 <head></head>
 <body>
 <?php
-$today = "2019-5-21";
-$date1 = "2019-8-1";
-$date2 = "2019-8-31";
-if(($today > $date1) && ($today < $date2))
+$today = getdate();
+$start = new DateTime('2019-08-01');
+$end = new DateTime('2019-08-31');
+if(($today > $start) && ($today < $end))
 echo "It's August so it's really hot.\n";
 else echo "Not August, so at least not in the peak of the heat.\n";
  ?>
